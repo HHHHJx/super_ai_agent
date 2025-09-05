b507592180e8eda970877df4a9b3b71b17c91802
HHHHJx <954525281@qq.com>
2025-09-05 21:44:48 +0800

    AI恋爱大师的初步开发

---
+++ src/main/java/com/jx/superaiagent/rag/LoveAppDocumentLoader.java
@@ -0,0 +1,46 @@
+package com.jx.superaiagent.rag;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
+import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Component;
+
+import org.springframework.ai.document.Document;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Slf4j
+public class LoveAppDocumentLoader {
+    private final ResourcePatternResolver resourcePatternResolver;
+
+    LoveAppDocumentLoader(ResourcePatternResolver resourcePatternResolver) {
+        this.resourcePatternResolver = resourcePatternResolver;
+    }
+
+    public List<Document> loadMarkdowns(){
+        List<Document> allDocuments = new ArrayList<>();
+        try {
+            // 获取文档来源。
+            Resource[] resources = resourcePatternResolver.getResources("classpath:document/*.md");
+            for (Resource resource : resources) {
+                String fileName = resource.getFilename();
+                // 使用DocumentReader逐个读取文档。
+                MarkdownDocumentReaderConfig config = MarkdownDocumentReaderConfig.builder()
+                        .withHorizontalRuleCreateDocument(true)
+                        .withIncludeCodeBlock(false)
+                        .withIncludeBlockquote(false)
+                        .withAdditionalMetadata("filename", fileName) // 额外的元信息：文件名。
+                        .build();
+                MarkdownDocumentReader reader = new MarkdownDocumentReader(resource, config);
+                allDocuments.addAll(reader.get());
+            }
+        } catch (IOException e) {
+            log.error("Markdown 文档加载失败", e);
+        }
+        return allDocuments;
+    }
+}
